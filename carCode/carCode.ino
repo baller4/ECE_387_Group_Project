435961876d443fd164372356b43638859fc316f5
steigeia <steigeia@miamioh.edu>
2017-05-03 17:46:11 -0400

    Update carCode.ino

--- carCode/carCode.ino
+++ carCode/carCode.ino
@@ -19,16 +19,12 @@ char dataReceived[2]; // this must match dataToSend in the TX
 
 
 void setup() {
-  int a = -1;
+  //int a = -1;
   Serial.begin(250000);
-  Serial.println(a,HEX);
+  //Serial.println(a,HEX);
   
   Serial.println("SimpleRx Starting");
   motor.begin();
-  startRadio();
-}
-
-void startRadio(){
   radio.begin();
   radio.setDataRate(RF24_250KBPS);
   radio.setChannel(108);
@@ -39,39 +35,35 @@ void startRadio(){
   radio.startListening();
 }
 
+
 void loop() {
-  delay(200);
   readData();
-  delay(200);
+  delay(100);
   sendD();
+  delay(100);
   
     
 }
 
 void readData() {
+  Serial.println("read");
 //  while
 //  startRadio();
   if(radio.available()) {
     
     //read the data and store it in dataReceived
     radio.read(&dataReceived, sizeof(dataReceived));
-//    if(dataReceived[0] == -1){
-//          motor.speed(0, 100);
-//          motor.speed(1, 100);
-//      }else{
-//        motor.brake(0);
-//        motor.brake(1);
-//      }
-  dataReceived[0] = 3;
-  dataReceived[1] = 0;
-  move(dataReceived);
+    move(dataReceived);
       //show the read data
     showData();
+  }else{
+    Serial.println("nothing recieved");
   }
   
 }
 
 void move(char * data) {
+  Serial.println("move");
   int speed0,speed1;
   switch(data[0]){
     case 0:
@@ -97,26 +89,38 @@ void move(char * data) {
   }
   switch(data[1]){
     case -3:
-    speed1-=30;
+    if(speed1!=0){
+      speed1-=45;
+    }
     break;
     case -2:
-    speed1-=20;//=map(speed1,0,100,0,50);
+    if(speed1!=0){
+      speed1-=30;//=map(speed1,0,100,0,50);
+    }
     break;
     case -1:
-    speed1-=10;//map(speed1,0,100,0,75);
+    if(speed1!=0){
+      speed1-=15;//map(speed1,0,100,0,75);
+    }
     break;
     case 0:
     speed0 = speed0;
     speed1 = speed1;
     break;      
     case 1:
-    speed0-=10;//map(speed0,0,100,0,75);
+    if(speed0!=0){
+      speed0-=15;//map(speed0,0,100,0,75);
+    }
     break;
     case 2:
-    speed0-=20;//map(speed0,0,100,0,50);
+    if(speed0!=0){
+      speed0-=30;//map(speed0,0,100,0,50);
+    }
     break;
     case 3:
-    speed0-=30;//map(speed0,0,100,0,25);
+    if(speed0!=0){
+    speed0-=45;//map(speed0,0,100,0,25);
+    }
     break;
     default:
     Serial.println("error");
@@ -126,16 +130,9 @@ void move(char * data) {
   digitalWrite(MOTORSHIELD_IN4, LOW);
   Serial.println(speed0);
   Serial.println(speed1);
-//  motor.speed(0, 100);
-//  motor.speed(1, 100);
-//  delay(1);
-//  motor.speed(0, speed0);
-//  motor.speed(1, speed1);
   if(speed0==0){
     motor.brake(0);
   }else{
-//    motor.speed(0, 100);
-//    delay(1);
     motor.speed(0, speed0);
   }
   if(speed1==0){
@@ -156,28 +153,32 @@ void showData() {
 }
 
 void sendD() {
+  Serial.println("send");
 //  startRadio();
-  char dataToSend[1];
-  if(Ultrasonic.distanceRead()<50)
-  {
-    dataToSend[0]=1;
-  }else{
-    dataToSend[0]=0;
-  }
+  char dataToSend[1] = {0};
+//  if(Ultrasonic.distanceRead()<50)
+//  {
+//    dataToSend[0]=1;
+//  }else{
+//    dataToSend[0]=0;
+//  }
   
   radio.stopListening();
+  
   bool rslt;
   rslt = radio.write(&dataToSend, sizeof(dataToSend));
+  Serial.println("send1");
         // Always use sizeof() as it gives the size as the number of bytes.
         // For example if dataToSend was an int sizeof() would correctly return 2
   radio.startListening();
+  Serial.println("send2");
   Serial.print("Data Sent ");
   Serial.print(dataToSend[0],DEC);
   if (rslt) {
     Serial.println("  Acknowledge received");
   }
   else {
-    delay(50);
+    delay(15);
     Serial.println("  Tx failed");
   }
 }
